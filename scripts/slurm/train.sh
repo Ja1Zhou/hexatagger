d1bcc0b8b563e2c4c02fce048dd8a3c8b766dc03
Jay Zhou <zhejianz@usc.edu>
2023-10-22 16:58:13 -0700

    attempt mem fix

--- scripts/slurm/train.sh
+++ scripts/slurm/train.sh
@@ -2,10 +2,11 @@
 #SBATCH --account=jonmay_231
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=a100:1
 #SBATCH --constraint=a100-80gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
 #SBATCH --output=outputs/output.txt
 #SBATCH --error=outputs/error.txt
 
