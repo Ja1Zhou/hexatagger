5b9bb5f437b6a0714b4790f96e1cfa9197590fd9
Jeremiah <zhejianz@usc.edu>
2023-12-06 18:11:32 +0800

    update for a100 test

--- scripts/slurm/ptb_eval.sh
+++ scripts/slurm/ptb_eval.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=jonmay_231
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --gpus-per-task=a40:1
+#SBATCH --gpus-per-task=a100:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
